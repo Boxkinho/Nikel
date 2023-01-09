4e4434f0fcd3cdba7f083333d853ab0d9a0a802d
Boxkinho <jbmmf@yahoo.com.br>
2023-01-09 19:01:50 -0300

    Iniciando o Git

---
+++ public/js/index.js
@@ -0,0 +1,34 @@
+const myModal = new bootstrap.Modal("#register-modal");
+
+// CRIAR CONTA
+
+document.getElementById("create-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email-create-input").value;
+    const password = document.getElementById("password-create-input").value;
+
+    if(email.length < 5) {
+        alert("Preencha o campo com um e-mail válido.");
+        return;
+    }
+
+    if(password.length < 4) {
+        alert("Preencha o campo com no mínimo 4 dígitos.");
+        return;
+    }
+
+    saveAccount({
+        login: email,
+        password: password,
+        transactions: []
+    });
+
+    myModal.hide();
+
+    alert("Conta criada com sucesso!");
+});
+
+function saveAccount(data) {
+    localStorage.setItem(data.login, JSON.stringify(data));
+}
