acd6fd5512bd03c44d954f5c0dfa93afd37fa574
Boxkinho <jbmmf@yahoo.com.br>
2023-01-10 06:35:02 -0300

    Aplicação concluída

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,36 @@
 const myModal = new bootstrap.Modal("#register-modal");
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+
+checkLogged();
+
+//LOGAR NO SISTEMA
+
+document.getElementById("login-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email-input").value;
+    const password = document.getElementById("password-input").value;
+    const checkSession = document.getElementById("session-check").checked;
+
+    const account = getAccount(email);
+
+    if(!account) {
+        alert("Ops!! Verifique o usuário ou a senha!");
+        return;
+    }
+
+    if(account) {
+        if(account.password !== password) {
+            alert("Ops!! Verifique o usuário ou a senha!");
+            return;
+        }
+
+        saveSession(email, checkSession);
+
+        window.location.href = "home.html";
+    }
+});
 
 // CRIAR CONTA
 
@@ -29,6 +61,37 @@ document.getElementById("create-form").addEventListener("submit", function(e) {
     alert("Conta criada com sucesso!");
 });
 
+function checkLogged() {
+    if(session) {
+        sessionStorage.setItem("logged", session);
+        logged = session;
+    }
+
+    if(logged) {
+        saveSession(logged, session);
+
+        window.location.href = "home.html";
+    }
+}
+
 function saveAccount(data) {
     localStorage.setItem(data.login, JSON.stringify(data));
+}
+
+function saveSession(data, saveSession) {
+    if(saveSession) {
+        localStorage.setItem("session", data);
+    }
+
+    sessionStorage.setItem("logged", data);
+}
+
+function getAccount(key) {
+    const account = localStorage.getItem(key);
+
+    if(account) {
+        return JSON.parse(account);
+    }
+
+    return "";
 }
